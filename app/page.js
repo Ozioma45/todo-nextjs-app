8abb40d8facb094e1f73e5a0af82ce5352710d10
Ozioma45 <oziomaegole@gmail.com>
2024-04-03 17:50:21 +0100

    start building the todo-app and created the template for the app

--- app/page.js
+++ app/page.js
@@ -1,5 +1,59 @@
-/* export default function Home() {
+"use client";
+import Todo from "@/Components/Todo";
+import { useState } from "react";
+
+export default function Home() {
+  const [formData, setFromData] = useState({
+    title: "",
+    descriptiom: "",
+  });
+
   return (
-    
-  //);
-} */
+    <>
+      <form className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto">
+        <input
+          type="text"
+          name="title"
+          placeholder="Enter Title"
+          className="px-3 py-2 border-2 w-full"
+        />
+        <textarea
+          name="description"
+          placeholder="Enter Description"
+          className="px-3 py-2 border-2 w-full"></textarea>
+        <button type="submit" className="bg-orange-600 py-3 px-11 text-white">
+          Add Todo
+        </button>
+      </form>
+
+      <div className="relative overflow-x-auto mt-24 w-[60%] mx-auto">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                ID
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Title
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Description
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Status
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <Todo />
+            <Todo />
+            <Todo />
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
