cc6afa992ee50a47eda5745b025b3e010f94f294
Ozioma45 <oziomaegole@gmail.com>
2024-04-03 18:07:52 +0100

    added the onchange handler

--- app/page.js
+++ app/page.js
@@ -3,21 +3,32 @@ import Todo from "@/Components/Todo";
 import { useState } from "react";
 
 export default function Home() {
-  const [formData, setFromData] = useState({
+  const [formData, setFormData] = useState({
     title: "",
-    descriptiom: "",
+    description: "",
   });
+
+  const onChangeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormData((form) => ({ ...form, [name]: value }));
+    console.log(formData);
+  };
 
   return (
     <>
       <form className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto">
         <input
+          value={formData.title}
+          onChange={onChangeHandler}
           type="text"
           name="title"
           placeholder="Enter Title"
           className="px-3 py-2 border-2 w-full"
         />
         <textarea
+          value={formData.description}
+          onChange={onChangeHandler}
           name="description"
           placeholder="Enter Description"
           className="px-3 py-2 border-2 w-full"></textarea>
